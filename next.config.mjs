6e31aa583b0974e22c317a1695b050510d8f9e06
marketing <marketing.critidesign@gmail.com>
2026-08-04 12:51:47 +0530

    optimise config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
